a1e28eca847e041752635738bdebea6df3a56d3f
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-06-27 21:39:37 -0500

    Add project files.

---
+++ MVC/Controllers/GradeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class GradeController : Controller
+    {
+        public IActionResult Welcome(int? firstValue, int? secondvalue)
+        {
+            ViewBag.Grade = firstValue + secondvalue;
+            ViewBag.IsCurrent = true;
+            return View();
+        }
+    }
+}
---
+++ MVC/Models/ContentPost.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC.Models
+{
+    public class ContentPost
+    {
+        public int Id { get; set; }
+        public string Contents { get; set;}
+        public ContentPost(string contents)
+        {
+            Contents = contents;
+        }
+    }
+}
